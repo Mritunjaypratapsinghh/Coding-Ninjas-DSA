ece56ea6ec479912abaeda3d9ba995cc6ef45cc6
Mritunjay Pratap Singh <71664291+Mritunjaypratapsinghh@users.noreply.github.com>
2023-05-29 11:21:36 +0530

    git commit

---
+++ Pattern 1/Code : Reverse Number Pattern.py
@@ -0,0 +1,46 @@
+# Code : Reverse Number Pattern
+# Send Feedback
+# Print the following pattern for the given N number of rows.
+# Pattern for N = 4
+# 1
+# 21
+# 321
+# 4321
+# Input format :
+# Integer N (Total no. of rows)
+# Output format :
+# Pattern in N lines
+# Constraints
+# 0 <= N <= 50
+# Sample Input 1:
+# 5
+# Sample Output 1:
+# 1
+# 21
+# 321
+# 4321
+# 54321
+# Sample Input 2:
+# 6
+# Sample Output 2:
+# 1
+# 21
+# 321
+# 4321
+# 54321
+# 654321
+
+
+n=int(input())
+i=1
+while i<=n:
+    j=1
+    k=i
+    while j<=i:
+        print(k,end="")
+        k-=1
+        j+=1
+    print()
+    i+=1
+    
+        
