369a7fac49b90cac4a2eb3c532791a91798fe75c
Mritunjay Pratap Singh <71664291+Mritunjaypratapsinghh@users.noreply.github.com>
2023-05-30 10:43:13 +0530

    git commit

---
+++ Pattern 1/Code : Interesting Alphabets.py
@@ -0,0 +1,49 @@
+# Code : Interesting Alphabets
+
+# Print the following pattern for the given number of rows.
+# Pattern for N = 5
+# E
+# DE
+# CDE
+# BCDE
+# ABCDE
+# Input format :
+# N (Total no. of rows)
+# Output format :
+# Pattern in N lines
+# Constraints
+# 0 <= N <= 26
+# Sample Input 1:
+# 8
+# Sample Output 1:
+# H
+# GH
+# FGH
+# EFGH
+# DEFGH
+# CDEFGH
+# BCDEFGH
+# ABCDEFGH
+# Sample Input 2:
+# 7
+# Sample Output 2:
+# G
+# FG
+# EFG
+# DEFG
+# CDEFG
+# BCDEFG
+# ABCDEFG
+
+
+n=int(input())
+i=1
+while i<=n:
+    j=1
+    start=chr(64+1+n-i)
+    while j<=i:
+        charp=chr(ord(start)+j-1)
+        print(charp, end="")
+        j+=1
+    print( )
+    i+=1
